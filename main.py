a398f9ebd4302f830e476de960226d86be426c5f
YarkoMarko <Yarema1310@gmail.com>
2023-11-15 20:23:39 +0200

    Test CircleInSquare

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-class Circle:
-    def __init__(self):
-        print("Circle")
-
-
 class Square:
     def __init__(self):
-        print("Square")
+        self._side = int(input("Enter side of square: "))
 
 
-class CircleInSquare(Circle, Square):
+class Circle:
+    _radius = 0
+
+
+class CircleInSquare(Square, Circle):
     def __init__(self):
         super().__init__()
-        Square.__init__(self)
-        print("Circle in square")
+        Circle.__init__(self)
+        self._radius = self._side / 2
+        print(3.14 * (self._radius ** 2))
 
 
 c = CircleInSquare()
