9a2348efc3aefd37aff218c0ceeadce68338bba9
YarkoMarko <Yarema1310@gmail.com>
2023-11-15 20:29:44 +0200

    Test CircleInSquare

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import pi
+
 class Square:
     def __init__(self):
         self._side = int(input("Enter side of square: "))
@@ -12,7 +14,7 @@ class CircleInSquare(Square, Circle):
         super().__init__()
         Circle.__init__(self)
         self._radius = self._side / 2
-        print(3.14 * (self._radius ** 2))
+        print(pi * (self._radius ** 2))
 
 
 c = CircleInSquare()
